22d8a3775d283b16ba1b8d995238a9d9bff5c187
BURNSIDE HIGH SCHOOL <code@burnside.school.nz>
2025-05-29 11:52:44 +1200

    skibidi camera

--- scripts/camera.gd
+++ scripts/camera.gd
@@ -3,9 +3,19 @@ extends Camera2D
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
+	position.y = 350
+	
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	pass
+	var xbob = get_parent().get_node("Bob").position.x
+	var xrob = get_parent().get_node("Rob").position.x
+	position.x = (xbob + xrob) / 2
+	
+	if abs(xrob - xbob) >= 1000:
+		zoom.x = abs(1000 / (xrob - xbob))
+		zoom.y = abs(1000 / (xrob - xbob))
+	else:
+		zoom.x = 1
+		zoom.y = 1
