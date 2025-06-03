c153f2dfa57f6a13bb437ab6943f04acfe30df0e
BURNSIDE HIGH SCHOOL <code@burnside.school.nz>
2025-06-03 14:38:52 +1200

    skibidi camera

--- scripts/rob.gd
+++ scripts/rob.gd
@@ -6,6 +6,8 @@ const JUMP_VELOCITY = -550.0
 
 
 func _physics_process(delta):
+	var xbob = get_parent().get_node("Bob").position.x
+	var xrob = get_parent().get_node("Rob").position.x
 	# Add the gravity.
 	if not is_on_floor():
 		velocity += get_gravity() * delta
@@ -17,7 +19,7 @@ func _physics_process(delta):
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
 	var direction = Input.get_axis("A", "D")
-	if direction:
+	if direction and abs(xbob - xrob) <= 1500:
 		velocity.x = direction * SPEED
 	else:
 		velocity.x = move_toward(velocity.x, 0, SPEED)
