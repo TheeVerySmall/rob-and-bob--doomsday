041cc917b2792fa7227fda475bd238033ad07925
BURNSIDE HIGH SCHOOL <code@burnside.school.nz>
2025-06-06 10:23:59 +1200

    Animation completed and speed/jump adjusted

--- scripts/bob.gd
+++ scripts/bob.gd
@@ -1,18 +1,34 @@
+#Bob is faster and has int
+
+
+
 extends CharacterBody2D
 
+@onready var anim : AnimatedSprite2D = $AnimatedSprite2D
 
 const SPEED = 300.0
-const JUMP_VELOCITY = -550.0
+const JUMP_VELOCITY = -400.0
 
 
 func _physics_process(delta):
 	# Add the gravity.
 	if not is_on_floor():
 		velocity += get_gravity() * delta
+	else:
+		if abs(velocity.x) > 10:
+			anim.play("run")
+		else:
+			anim.play("idle")
+	
+	if velocity.x < 0:
+		anim.flip_h = true
+	else:
+		anim.flip_h = false
 
 	# Handle jump.
 	if Input.is_action_just_pressed("ui_up") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
+		anim.play("jump")
 
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
