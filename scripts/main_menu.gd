7f6989ecf46a8fa7ea6946b4d565127d56b85414
BURNSIDE HIGH SCHOOL <code@burnside.school.nz>
2025-07-14 12:53:51 +1200

    MEnu

---
+++ scripts/main_menu.gd
@@ -0,0 +1,19 @@
+extends Node2D
+
+
+func _on_button_button_down() -> void:
+	get_tree().change_scene_to_file("res://scenes/level_select.tscn")
+
+
+func _on_button_2_button_down() -> void:
+	#get_tree().change_scene_to_file()
+	pass
+
+
+func _on_button_3_button_down() -> void:
+	#get_tree().change_scene_to_file()
+	pass
+
+
+func _on_button_4_button_down() -> void:
+	pass # Replace with function body.
