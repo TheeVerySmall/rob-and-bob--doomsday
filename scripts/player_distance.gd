262cf2cb73a5c398e15871fbf6681849b1ac126e
BURNSIDE HIGH SCHOOL <code@burnside.school.nz>
2025-08-12 14:40:01 +1200

    sigma

--- scripts/player_distance.gd
+++ scripts/player_distance.gd
@@ -3,6 +3,6 @@ extends ProgressBar
 func _physics_process(delta):
 	Globals.player_distance = abs(Globals.x_position_rob - Globals.x_position_bob)
 	value = Globals.player_distance
-	print(Globals.player_distance)
-	if Globals.player_distance > 700:
+	#print(Globals.player_distance)
+	if Globals.player_distance > 1600:
 		get_tree().change_scene_to_file("res://scenes/death_screen.tscn")
