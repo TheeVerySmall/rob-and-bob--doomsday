c09518314dd3a22ac9489b4080dba512c491c882
BURNSIDE HIGH SCHOOL <code@burnside.school.nz>
2025-09-03 14:21:03 +1200

    JBDWJIND

--- scripts/main_menu.gd
+++ scripts/main_menu.gd
@@ -6,7 +6,7 @@ func _on_button_button_down() -> void:
 
 
 func _on_button_2_button_down() -> void:
-	get_tree().change_scene_to_file("res://scenes/Temp_Tutorial.tscn")
+	get_tree().change_scene_to_file("res://scenes/tutorial_legit.tscn")
 
 
 func _on_button_3_button_down() -> void:
